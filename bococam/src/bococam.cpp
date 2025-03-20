16e5d1a34699a9747004da8412ca117b28f4da17
Kan <70838508+kan6868@users.noreply.github.com>
2025-03-20 14:23:29 +0700

    Update bococam.cpp

--- bococam/src/bococam.cpp
+++ bococam/src/bococam.cpp
@@ -1,7 +1,12 @@
-// myextension.cpp
+// bococam.cpp
 // Extension lib defines
 #define LIB_NAME "BocoCamera"
 #define MODULE_NAME "bococam"
+
+#define DISPLAY_WIDTH 1280.0
+#define DISPLAY_HEIGHT 720.0
+
+#define HALF_MULTIPLIER 0.5
 
 #include <dmsdk/sdk.h>
 
@@ -20,14 +25,19 @@ struct Camera
     dmGameObject::HInstance mainCam;   // Handle to the main camera object
     dmGameObject::HInstance worldTarget; // Handle to the world target
 
-    float windowWidth = 1280.0f;  // Window width for rendering
-    float windowHeight = 720.0f; // Window height for rendering
+    float windowWidth = (float)DISPLAY_WIDTH;  // Window width for rendering
+    float windowHeight = (float)DISPLAY_HEIGHT; // Window height for rendering
+
+    float halfWidth = windowWidth * HALF_MULTIPLIER; // Half of the window width
+    float halfHeight = windowHeight * HALF_MULTIPLIER; // Half of the window height
 
     float zoom = 1.0f;            // Zoom level of the camera
-    float worldScale = 1.0f;      // Scaling factor for the world
+    float aspect = 1.0f;          // Aspect ratio of the camera
+    
+    float invZoom = 1.0f / (zoom * aspect);  // Inverse of the zoom level
 
-    unsigned int displayWidth = 1280;  // Display width
-    unsigned int displayHeight = 720; // Display height
+    unsigned int displayWidth = DISPLAY_WIDTH;  // Display width
+    unsigned int displayHeight = DISPLAY_HEIGHT; // Display height
 };
 
 // Static global variables to hold camera and state data
@@ -50,17 +60,41 @@ static void Resize(const int& width, const int& height)
     float displayScaleX = g_Camera.windowWidth / g_Camera.displayWidth;
     float displayScaleY = g_Camera.windowHeight / g_Camera.displayHeight;
     
+    g_Camera.windowWidth = width;
+    g_Camera.windowHeight = height;
+
+    g_Camera.halfWidth = width * HALF_MULTIPLIER;
+    g_Camera.halfHeight = height * HALF_MULTIPLIER;
+
     // Find the minimum scale factor between X and Y, and apply the world scaling
-    float displayScale = fmin(displayScaleX, displayScaleY) * g_Camera.worldScale;
+    g_Camera.aspect = fmin(displayScaleX, displayScaleY);
+    
+    float scaleValue = g_Camera.zoom * g_Camera.aspect;
+
+    // Update the inverse zoom factor
+    g_Camera.invZoom = 1.0f / scaleValue;
     
     // Log the calculated scale factor
-    dmLogInfo("Scale: %f", displayScale);
+    dmLogInfo("Scale: %f", scaleValue);
 
     // Set the position of the camera (shifted to center the view)
-    dmGameObject::SetPosition(g_Camera.mainCam, Point3(width * -.5f, height * -.5f, 0.0f));
+    dmGameObject::SetPosition(g_Camera.mainCam, Point3(width * -HALF_MULTIPLIER, height * -HALF_MULTIPLIER, 0.0f));
 
     // Apply the calculated scale to the world target
-    dmGameObject::SetScale(g_Camera.worldTarget, Vector3(displayScale, displayScale, displayScale));
+    dmGameObject::SetScale(g_Camera.worldTarget, Vector3(scaleValue));
+}
+/**
+ * Remaps a value from one range to another.
+ * @param value The value to remap.
+ * @param low1 The low end of the value's current range.
+ * @param high1 The high end of the value's current range.
+ * @param low2 The low end of the value's target range.
+ * @param high2 The high end of the value's target range.
+ * @return The remapped value.
+ */
+static float remap(const float& value, const float& low1, const float& high1, const float& low2, const float& high2)
+{
+    return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
 }
 
 /**
@@ -96,13 +130,28 @@ static int InitCamera(lua_State* L)
     g_Camera.worldTarget = world;
 
     // Get the scale of the world target and use it as the world scale
-    g_Camera.worldScale = dmGameObject::GetScale(world).getX();
-
+    g_Camera.zoom = dmGameObject::GetScale(world).getX();
+    // g_Camera.worldScale = g_Camera.initScale;
     // Resize the camera's viewport based on the initial window size
     Resize(g_Camera.windowWidth, g_Camera.windowHeight);
     
     return 0;
 }
+
+/**
+ * Sets the zoom level of the camera and updates the world target's scale accordingly.
+ * @param number zoom The new zoom level for the camera.
+ * @return 0 This function does not return any value.
+ */
+static int Zoom(lua_State* L)
+{
+    float zoom = lua_tonumber(L, 1);
+    g_Camera.zoom = zoom;
+    
+    dmGameObject::SetScale(g_Camera.worldTarget, Vector3(zoom));
+    return 0;
+}
+
 /**
  * Releases the camera and world target game objects, and marks the camera system as inactive.
  * 
@@ -112,7 +161,7 @@ static int InitCamera(lua_State* L)
  * and updating the camera's state to inactive.
  */
 // Function to release the camera and reset the state
-static int ReleaseCamera()
+static int ReleaseCamera(lua_State* L)
 {
     // Reset the camera and world target instances
     g_Camera.mainCam = nullptr;
@@ -140,11 +189,78 @@ static int ResizeCamera(lua_State* L)
     // Get the new window size from the Lua stack
     g_Camera.windowWidth = lua_tonumber(L, 1);
     g_Camera.windowHeight = lua_tonumber(L, 2);
-
+    
     // Resize the camera's viewport based on the new window size
     Resize(g_Camera.windowWidth, g_Camera.windowHeight);
     
     return 0;
+}
+
+static int ScreenToWorld(lua_State* L)
+{
+    // Check if the camera system is active
+    if (!g_State.isActive)
+    {
+        // If inactive, return nil to the Lua stack
+        lua_pushnil(L);
+        return 1;
+    }
+    
+    float invZoomHalfWidth = g_Camera.invZoom * g_Camera.halfWidth;
+    float invZoomHalfHeight = g_Camera.invZoom * g_Camera.halfHeight;
+
+    dmVMath::Vector3* out = dmScript::CheckVector3(L, 1);
+    float screenX = out->getX();
+    float screenY = out->getY();
+
+    screenX = remap(screenX, -g_Camera.halfWidth, g_Camera.halfWidth, -invZoomHalfWidth, invZoomHalfWidth);
+    screenY = remap(screenY, -g_Camera.halfHeight, g_Camera.halfHeight, -invZoomHalfHeight, invZoomHalfHeight);
+
+    out->setX(screenX);
+    out->setY(screenY);
+
+    dmScript::PushVector3(L, *out);
+    return 1;
+}
+/**
+ * Retrieves the world position of a given game object by factoring in its local position, world position,
+ * scale, and rotation.
+ * @param URL|ID instance The game object instance for which the world position is requested.
+ * @return 1 The function returns the calculated world position as a `Vector3` on the Lua stack.
+ */
+static int LocalToWorld(lua_State* L)
+{
+    // Check if the camera system is active
+    if (!g_State.isActive)
+    {
+        // If inactive, return nil to the Lua stack
+        lua_pushnil(L);
+        return 1;
+    }
+
+    // Get the game object instance for which the world position is requested
+    dmGameObject::HInstance instance = dmScript::CheckGOInstance(L, 1);
+
+    // Retrieve the world rotation, world position, world scale, and local position of the object
+    const Quat& rotation    = dmGameObject::GetWorldRotation(instance);
+    const Point3& position  = dmGameObject::GetWorldPosition(instance);
+    const Vector3& scale    = dmGameObject::GetWorldScale(instance);
+    const Point3& localPosition  = dmGameObject::GetPosition(instance);
+
+    Vector3 vecResult ;
+
+    vecResult.setX(localPosition.getX() * scale.getX());
+    vecResult.setY(localPosition.getY() * scale.getY());
+    vecResult.setZ(localPosition.getZ() * scale.getZ());
+
+    vecResult = dmVMath::Rotate(rotation, vecResult);
+
+    vecResult.setX(vecResult.getX() + position.getX());
+    vecResult.setY(vecResult.getY() + position.getY());
+    vecResult.setZ(vecResult.getZ() + position.getZ());
+
+    dmScript::PushVector3(L, vecResult);
+    return 1;
 }
 
 /**
@@ -159,7 +275,7 @@ static int ResizeCamera(lua_State* L)
  * It then applies the inverse of the object's rotation and scales the result by the object's world scale.
  * This provides the accurate world position, which is then returned to the Lua script.
  */
-static int GetWorldPosition(lua_State* L)
+static int WorldToLocal(lua_State* L)
 {
     // Check if the camera system is active
     if (!g_State.isActive)
@@ -188,7 +304,7 @@ static int GetWorldPosition(lua_State* L)
     vecResult.setX(localPosition.getX() + position.getX());
     vecResult.setY(localPosition.getY() + position.getY());
     vecResult.setZ(localPosition.getZ() + position.getZ());
-    
+
     // Apply the inverse rotation to the position to get the position in the local space
     vecResult = dmVMath::Rotate(invQuat, vecResult);
 
@@ -206,10 +322,13 @@ static int GetWorldPosition(lua_State* L)
 // Functions exposed to Lua
 static const luaL_reg Module_methods[] =
 {
-	{"get_world_position", GetWorldPosition},
-	{"init_camera", InitCamera},
-	// {"release_camera", ReleaseCamera},
-	{"resize", ResizeCamera},
+    {"init_camera", InitCamera},
+    {"local_to_world", LocalToWorld},
+    {"resize", ResizeCamera},
+    {"release_camera", ReleaseCamera},
+    {"screen_to_world", ScreenToWorld},
+    {"world_to_local", WorldToLocal},
+    {"zoom", Zoom},
 	{0, 0}
 };
 static void LuaInit(lua_State* L)
@@ -225,10 +344,11 @@ static void LuaInit(lua_State* L)
 
 static dmExtension::Result AppInitializeMyExtension(dmExtension::AppParams* params)
 {
-	g_Camera.displayWidth = dmConfigFile::GetInt(params->m_ConfigFile, "display.width", 1280);
-	g_Camera.displayHeight = dmConfigFile::GetInt(params->m_ConfigFile, "display.height", 720);
+	g_Camera.displayWidth = dmConfigFile::GetInt(params->m_ConfigFile, "display.width", DISPLAY_WIDTH);
+	g_Camera.displayHeight = dmConfigFile::GetInt(params->m_ConfigFile, "display.height", DISPLAY_HEIGHT);
 	dmLogInfo("AppInitializeMyExtension: %d %d", g_Camera.displayWidth, g_Camera.displayHeight);
-	return dmExtension::RESULT_OK;
+
+    return dmExtension::RESULT_OK;
 }
 
 static dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
